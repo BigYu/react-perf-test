90064450f9d983c9f81d423cf14b682bee0a310f
BigYu <jinyucong911@outlook.com>
2018-10-08 16:09:40 +0800

    fix

--- MyTodoApp/src/components/item-list.js
+++ MyTodoApp/src/components/item-list.js
@@ -1,28 +1,32 @@
 import React from 'react';
 
-export default function({ items, searchText }) {
-  const tbody = items.filter(item => item.text.indexOf(searchText) !== -1).map((item, index) => (
-    <tr key={index}>
-      <td>{item.text}</td>
-      <td>{item.createdAt}</td>
-    </tr>
-  ));
+export default class ItemList extends React.Component {
+  render() {
+    const { items, searchText } = this.props;
 
-  return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>
-            text
-          </th>
-          <th>
-            createdAt
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {tbody}
-      </tbody>
-    </table>
-  );
+    const tbody = items.filter(item => item.text.indexOf(searchText) !== -1).map((item, index) => (
+      <tr key={index}>
+        <td>{item.text}</td>
+        <td>{item.createdAt}</td>
+      </tr>
+    ));
+
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>
+              text
+            </th>
+            <th>
+              createdAt
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {tbody}
+        </tbody>
+      </table>
+    );
+  }
 }
--- MyTodoApp/src/components/search-box.js
+++ MyTodoApp/src/components/search-box.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
-export default function SearchBox({ searchText, onChange }) {
-  return (
-    <div>
-      <input
-        className="form-control"
-        placeholder="Search..."
-        value={searchText}
-        onChange={(e) => {
-          e.preventDefault();
-          onChange(e.target.value);
-        }}
-      />
-    </div>
-  );
+export default class SearchBox extends React.Component {
+  render() {
+    const { searchText, onChange } = this.props;
+
+    return (
+      <div>
+        <input
+          className="form-control"
+          placeholder="Search..."
+          value={searchText}
+          onChange={(e) => {
+            e.preventDefault();
+            onChange(e.target.value);
+          }}
+        />
+      </div>
+    );
+  }
 }
